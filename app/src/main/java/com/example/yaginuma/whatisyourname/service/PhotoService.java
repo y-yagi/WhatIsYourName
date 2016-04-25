92022698ad4fb153ca0af35e8e37309fa1d89771
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-04-26 08:41:19 +0900

    add service class for retrofit

---
+++ app/src/main/java/com/example/yaginuma/whatisyourname/service/PhotoService.java
@@ -0,0 +1,17 @@
+package com.example.yaginuma.whatisyourname.service;
+
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+/**
+ * Created by yaginuma on 16/04/26.
+ */
+public interface PhotoService {
+    @Multipart
+    @POST("photo/info")
+    Call<ResponseBody> getInfoFromFile(@Part MultipartBody.Part file);
+}
